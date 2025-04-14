e1f4beb3d873ced79ba7402e3b5426a6b6c8793f
blbergo <bryan.l.bergo@gmail.com>
2025-04-14 12:52:17 -0700

    feat: model inference formatted output

--- src/app/api/model/inference/route.ts
+++ src/app/api/model/inference/route.ts
@@ -12,23 +12,31 @@ export async function POST(req: Request) {
     const res = await req.json();
     const titles: Content[] = JSON.parse((await fs.readFile(titleJsonPath, "utf-8")));
     const transformedTitles: TransformedContentVector[] = JSON.parse((await fs.readFile(transformedTitleJsonPath, "utf-8")));
-
     const { title, titleId, filters } = res;
 
     const session = await ort.InferenceSession.create(modelPath);
-
     const data = getFeatures({ title, titleId, content: titles, transformedContent: transformedTitles });
     const inputTensor = new ort.Tensor("float32", new Float32Array(data), [1, data.length]);
     const feeds = { float_input: inputTensor };
+
     const output = await session.run(feeds);
-    
-    const results = output["index"].data as Float32Array;
-    console.log(results);
-    console.log(transformedTitles.indexOf(results));
+    const outputTensor = output["index"].data as BigInt64Array;
+    const outputArray = Array.from(outputTensor).map((item) => Number(item));
+    const outputTitles = outputArray.map((item) => titles[item]).filter((item) => {
+        if (filters) {
+            const { genres, production_countries, release_year } = filters;
+            const isGenreMatch = genres ? item.genres.some((genre) => genres.includes(genre)) : true;
+            const isCountryMatch = production_countries ? item.production_countries.some((country) => production_countries.includes(country)) : true;
+            const isYearMatch = release_year ? item.release_year === release_year : true;
+            return isGenreMatch && isCountryMatch && isYearMatch;
+        }
+        return true;
+    });
+
 
     await session.release();
 
     return new Response(JSON.stringify({
-        message: "Hello from the API!"
+        data: outputTitles.slice(1),
     }))
 }
