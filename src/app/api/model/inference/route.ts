4e1b3973d2142f00924d905265d34149c972d43b
blbergo <bryan.l.bergo@gmail.com>
2025-04-14 11:04:17 -0700

    chore: refactor

--- src/app/api/model/inference/route.ts
+++ src/app/api/model/inference/route.ts
@@ -5,7 +5,6 @@ export async function GET() {
     const modelPath = path.join(process.cwd(), "../model/", "model.onnx")
     const session = await ort.InferenceSession.create(modelPath);
 
-
     await session.release();
 
     return new Response(JSON.stringify({
